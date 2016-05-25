bed1f48367d3f0288835e0183a7742708ff001bf
Shaina Russell <shaina.russell@students.mq.edu.au>
2016-05-25 17:08:55 +1000

    Fork from FAIMS-Tools

--- generators/christian/wireframe/wireframeElements.sh
+++ generators/christian/wireframe/wireframeElements.sh
@@ -1,21 +1,45 @@
+#!/bin/bash
 
-rmdir -rf wireframeImages/
-mkdir -p wireframeImages
-
-
-  ./makeElement.sh style_orientation_orientation "horizontal" input 1 false false false false false wireframeImages/
-  
-  ./makeElement.sh style_even_layout_weight "1" input 1 false false false false false wireframeImages/
-  
-  ./makeElement.sh style_large_layout_weight "3" input 1 false false false false false wireframeImages/
-  
-  ./makeElement.sh Tabgroup_Tab_Dummy "Dummy" input 1 false false true false false wireframeImages/
-  
-  ./makeElement.sh Tabgroup_Tab_Dropdown "Dropdown" dropdown 1 false false true false false wireframeImages/
-  
-  ./makeElement.sh Tabgroup_Tab_List "List" list 1 false false true false false wireframeImages/
-  
-  ./makeElement.sh Tabgroup_Tab_Picture "Picture" pictureGallery 1 false false true false false wireframeImages/
-  
-  ./makeElement.sh Tabgroup_Tab_Radio "Radio" radio 1 false false true false false wireframeImages/
-  
+./makeElement.sh style_orientation_orientation "horizontal" input 1 false false false false false .
+./makeElement.sh style_even_layout_weight "1" input 1 false false false false false .
+./makeElement.sh style_large_layout_weight "3" input 1 false false false false false .
+./makeElement.sh User_User_Select_User "Select User" dropdown 1 false false false false false .
+./makeElement.sh User_User_Login "Login" button 1 false false false false false .
+./makeElement.sh Control_Main_Record_New_Site "Record New Site" button 1 false false false false false .
+./makeElement.sh Control_Main_GPS_Diagnostics "GPS Diagnostics" input 1 false false false false true .
+./makeElement.sh Control_Search_Search_Term "Search Term" input 2 false false false false false .
+./makeElement.sh Control_Search_Search_Button "Search" button 2 false false false false false .
+./makeElement.sh Control_Search_Entity_Types "Entity Types" dropdown 1 false false false false false .
+./makeElement.sh Control_Search_Entity_List "Entity List" list 1 false false false false false .
+./makeElement.sh Site_Site_Title "Title" input 1 false false true true false .
+./makeElement.sh Site_Site_Latitude "Latitude" input 2 false false true false true .
+./makeElement.sh Site_Site_Northing "Northing" input 2 false false true false true .
+./makeElement.sh Site_Site_Longitude "Longitude" input 2 false false true false true .
+./makeElement.sh Site_Site_Easting "Easting" input 2 false false true false true .
+./makeElement.sh Site_Site_Take_From_GPS "Take From GPS" button 1 false false false false false .
+./makeElement.sh Site_Site_Radius_of_Site "Radius of Site" input 1 false false true false false .
+./makeElement.sh Site_Site_Record_New_Turtle "Record New Turtle" button 1 false false false false false .
+./makeElement.sh Site_Site_List_of_site_turtles "List of site turtles" list 1 false false false false false .
+./makeElement.sh Turtle_Turtle_Turtle_Markings "Turtle Markings" input 1 false false true true false .
+./makeElement.sh Turtle_Turtle_Did_you_give_turtle_a_marking "Did you give turtle a marking" radio 1 false false true false false .
+./makeElement.sh Turtle_Turtle_Take_Carapace_Marking_photo "Take Carapace Marking photo" file 1 false false true false false .
+./makeElement.sh Turtle_Turtle_Button_Take_Carapace_Marking_photo "Attach Photograph" button 1 false false false false false .
+./makeElement.sh Turtle_Turtle_Record_New_Capture "Record New Capture" button 1 false false false false false .
+./makeElement.sh Turtle_Turtle_List_of_Turtle_Captures "List of Turtle Captures" list 1 false false false false false .
+./makeElement.sh Turtle_Measurement_Species "Species" dropdown 1 false false true false false .
+./makeElement.sh Turtle_Measurement_Turtle_measurement_Carapace_Length "Turtle measurement Carapace Length" input 1 false false true false false .
+./makeElement.sh Turtle_Measurement_Turtle_measurement_Carapace_Width "Turtle measurement Carapace Width" input 1 false false true false false .
+./makeElement.sh Turtle_Measurement_Turtle_measurement_Plastron_Length "Turtle measurement Plastron Length" input 1 false false true false false .
+./makeElement.sh Turtle_Measurement_Turtle_measurement_Plastron_Width "Turtle measurement Plastron Width" input 1 false false true false false .
+./makeElement.sh Turtle_Measurement_Turtle_sex "Turtle sex" dropdown 1 false false true false false .
+./makeElement.sh Capture_Capture_timestamp "Timestamp" input 1 false false true false true .
+./makeElement.sh Capture_Capture_Latitude "Latitude" input 2 false false true false true .
+./makeElement.sh Capture_Capture_Northing "Northing" input 2 false false true false true .
+./makeElement.sh Capture_Capture_Longitude "Longitude" input 2 false false true false true .
+./makeElement.sh Capture_Capture_Easting "Easting" input 2 false false true false true .
+./makeElement.sh Capture_Capture_Take_From_GPS "Take From GPS" button 1 false false false false false .
+./makeElement.sh Capture_Capture_Trapping_Method "Trapping Method" dropdown 1 false false true false false .
+./makeElement.sh Capture_Capture_Take_Carapace_photo "Take Carapace photo" file 1 false false true false false .
+./makeElement.sh Capture_Capture_Button_Take_Carapace_photo "Attach Photograph" button 1 false false false false false .
+./makeElement.sh Capture_Capture_Take_Plastrum_photo "Take Plastrum photo" file 1 false false true false false .
+./makeElement.sh Capture_Capture_Button_Take_Plastrum_photo "Attach Photograph" button 1 false false false false false .
